b45776e90f617a09e57023847b32a3a5ecef9063
ahmadou5 <ahmadlasauwal@gmail.com>
2024-09-14 19:28:48 +0100

    ehehe

--- src/components/ui/LoadingIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { ReloadIcon } from '@radix-ui/react-icons'
-
-const LoadingIcon = ({ className }: { className?: string }) => {
-  return <ReloadIcon className={`mr-2 h-4 w-4 animate-spin ${className}`} />
-}
-
-export default LoadingIcon
